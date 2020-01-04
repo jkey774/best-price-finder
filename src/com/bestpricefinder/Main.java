9947b8a2ebd31783154e79d70c2feefe8e554a12
Joseph Key <jkey3@brookdale.com>
2020-01-04 01:46:19 -0600

    init

---
+++ src/com/bestpricefinder/Main.java
@@ -0,0 +1,8 @@
+package com.bestpricefinder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    CompletableFuturesDemo.show();
+    }
+}
---
+++ src/com/bestpricefinder/Quote.java
@@ -0,0 +1,33 @@
+package com.bestpricefinder;
+
+import java.text.DecimalFormat;
+
+public class Quote {
+    private final String site;
+    private final float price;
+
+
+    public Quote(String site, float price) {
+        this.site = site;
+        this.price = price;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+
+
+    @Override
+    public String toString() {
+
+        return "Quote {" +
+                "site = '" + site + '\'' +
+                ", price = $" + String.format(java.util.Locale.US,"%.2f", price) +
+                '}';
+    }
+}
